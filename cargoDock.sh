0b3ba601e8341e93d520d8f26e1b838b67a7d00b
Jacob Sanford <jsanford@unb.ca>
2017-05-31 08:09:25 -0300

    Doc Updates, etc.

--- cargoDock.sh
+++ cargoDock.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Build and deploy Drupal docker containers to CoreOS endpoints using Fleet
+# and Amazon Simple Container Registry as a storage medium.
+#
+# There are three primary functions that could  be broken out someday into
+# standalone scripts at a future date:
+#
+#   Triage - Determine branch to build.
+#   Build - Build the container
+#   Deploy - Deploy to endpoint, optionally making modifications for non-prod.
+
+## Triage.
+
 if [ -z "$BRANCH_NAME" ]; then
   if [ -z "$BRANCH" ]; then
     echo "Branch not set in Github ping or build parameter"
@@ -22,6 +34,8 @@ if [ -z ${!DEPLOY_HOST_VAR+x} ]; then
 fi
 DEPLOY_HOST=${!DEPLOY_HOST_VAR}
 
+## Build.
+
 # Check out the correct branch and get latest changes.
 git checkout $BUILD_BRANCH
 git pull origin $BUILD_BRANCH
@@ -37,11 +51,13 @@ docker build -t ${SERVICE_NAME}:${BUILD_BRANCH} .
 docker tag ${SERVICE_NAME}:${BUILD_BRANCH} ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${SERVICE_NAME}:${BUILD_BRANCH}
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${SERVICE_NAME}:${BUILD_BRANCH}
 
+## Deploy.
+
 # Get current fleet-unit files.
 rm -rf fleet-units
 git clone -b ${FLEET_UNIT_REPO_BRANCH} ${FLEET_UNIT_REPO_URI} fleet-units
 
-## Branch based transforms
+# Branch based transforms for non-prod servers
 if [ "$BUILD_BRANCH" -eq "dev" ]; then
   # MySQL
   sed -i "s|MYSQL_HOSTNAME=mysql.lib.unb.ca|MYSQL_HOSTNAME=mysqldev.lib.unb.ca|g" fleet-units/${SERVICE_NAME}.service
@@ -53,8 +69,7 @@ fi
 scp fleet-units/${SERVICE_NAME}.service ${DEPLOY_HOST}:/tmp/${SERVICE_NAME}.service
 rm -rf fleet-units
 
-## Using the fleet unit files, instruct the cluster to reload the new service.
-# For info, output a list of the units.
+# Using the fleet unit files, instruct the cluster to reload the new service.
 FLEETCTL_COMMAND="ssh $DEPLOY_HOST fleetctl"
 ${FLEETCTL_COMMAND} list-units
 
