1b2e97bb060d72d031305de601a70fa38e031793
Jacob Sanford <jsanford@unb.ca>
2017-06-07 14:12:41 -0300

    Simplify and convert to Kubernetes based control

--- cargoDock.sh
+++ cargoDock.sh
@@ -1,68 +1,70 @@
 #!/usr/bin/env bash
 
-if [ -z "$BRANCH_NAME" ]; then
-  if [ -z "$BRANCH" ]; then
+# Build and deploy Drupal docker containers to CoreOS endpoints using Fleet
+# and Amazon Simple Container Registry as a storage medium.
+#
+# There are three primary functions that could  be broken out someday into
+# standalone scripts at a future date:
+#
+#   Triage - Determine branch to build.
+#   Build - Build the container
+#   Deploy - Deploy to endpoint, optionally making modifications for non-prod.
+#
+# Required ENV Variables:
+#
+#   AWS_ACCOUNT_ID
+#   DOCKER_UPSTREAM_IMAGE
+#   SERVICE_NAME
+#
+
+AMAZON_ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
+KUBE_DEPLOYMENT_NAME=${SERVICE_NAME//./-}
+
+## Triage.
+##
+if [ -z "${BRANCH_NAME}" ]; then
+  if [ -z "${BRANCH}" ]; then
     echo "Branch not set in Github ping or build parameter"
     exit 1
   else
-    BUILD_BRANCH="$BRANCH"
+    BUILD_BRANCH="${BRANCH}"
   fi
 else
-  BUILD_BRANCH="$BRANCH_NAME"
+  BUILD_BRANCH="${BRANCH_NAME}"
 fi
 
-BUILD_BRANCH=$(echo $BUILD_BRANCH | sed 's|origin/||g')
-echo "$BUILD_BRANCH|$BRANCH_NAME|$BRANCH"
+BUILD_BRANCH=$(echo ${BUILD_BRANCH} | sed 's|origin/||g')
+echo "Building Branch ${BUILD_BRANCH}"
 
-# Get the target host.
-DEPLOY_HOST_VAR="DEPLOY_HOST_$BUILD_BRANCH"
-if [ -z ${!DEPLOY_HOST_VAR+x} ]; then
-  echo "Deploy host unset in ${DEPLOY_HOST_VAR}! Exiting."
-  exit 1;
-fi
-DEPLOY_HOST=${!DEPLOY_HOST_VAR}
-
+## Build.
+##
 # Check out the correct branch and get latest changes.
-git checkout $BUILD_BRANCH
-git pull origin $BUILD_BRANCH
+git checkout ${BUILD_BRANCH}
+git pull origin ${BUILD_BRANCH}
 git reset --hard HEAD
 git clean -f
 
 # Pull the latest version of the upstream image.
 docker pull ${DOCKER_UPSTREAM_IMAGE}
 
-# Build the image and push it to the registry.
-$(docker run -i -v $HOME/.aws:/home/aws/.aws unblibraries/aws-cli aws ecr get-login)
+# Build the theme(s).
+composer install
+vendor/bin/dockworker container:theme:build-all
+
+# Build the image and push it to the EC2 registry.
+$(docker run -i -v ${HOME}/.aws:/home/aws/.aws unblibraries/aws-cli aws ecr get-login)
 docker build -t ${SERVICE_NAME}:${BUILD_BRANCH} .
-docker tag ${SERVICE_NAME}:${BUILD_BRANCH} ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${SERVICE_NAME}:${BUILD_BRANCH}
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${SERVICE_NAME}:${BUILD_BRANCH}
+docker tag ${SERVICE_NAME}:${BUILD_BRANCH} ${AMAZON_ECR_URI}/${SERVICE_NAME}:${BUILD_BRANCH}
+IMAGE_SHA256_HASH=$(docker images --no-trunc ${AMAZON_ECR_URI}/${IMAGE_NAME}:${BUILD_BRANCH} --format "{{.ID}}")
+docker push ${AMAZON_ECR_URI}/${SERVICE_NAME}:${BUILD_BRANCH}
 
-# Get current fleet-unit files.
-rm -rf fleet-units
-git clone -b ${FLEET_UNIT_REPO_BRANCH} ${FLEET_UNIT_REPO_URI} fleet-units
+## Deploy.
+##
+# Update image hash to latest build.
+kubectl get deployment ${KUBE_DEPLOYMENT_NAME} --namespace=${BUILD_BRANCH} -o=yaml | sed "s|\(^\s*\)image: .*|\1image: $AMAZON_ECR_URI/$IMAGE_NAME@sha256:$IMAGE_SHA256_HASH|g" > /tmp/${KUBE_DEPLOYMENT_NAME}-new.yml
 
-## Branch based transforms
-if [ "$BUILD_BRANCH" -eq "dev" ]; then
-  # MySQL
-  sed -i "s|MYSQL_HOSTNAME=mysql.lib.unb.ca|MYSQL_HOSTNAME=mysqldev.lib.unb.ca|g" fleet-units/${SERVICE_NAME}.service
-  sed -i '|^    --log-|d' fleet-units/${SERVICE_NAME}.service
-  sed -i '|^    -e LOGZIO_KEY|d' fleet-units/${SERVICE_NAME}.service
-fi
+# Apply updated deployment.
+kubectl apply -f /tmp/${KUBE_DEPLOYMENT_NAME}-new.yml --record --namespace=${BUILD_BRANCH}
 
-# Send the appropriate fleet unit up to the server.
-scp fleet-units/${SERVICE_NAME}.service ${DEPLOY_HOST}:/tmp/${SERVICE_NAME}.service
-rm -rf fleet-units
-
-## Using the fleet unit files, instruct the cluster to reload the new service.
-# For info, output a list of the units.
-FLEETCTL_COMMAND="ssh $DEPLOY_HOST fleetctl"
-${FLEETCTL_COMMAND} list-units
-
-# Destroy the service.
-${FLEETCTL_COMMAND} destroy ${SERVICE_NAME}.service || true
-sleep 15
-
-# Submit and start the service.
-${FLEETCTL_COMMAND} submit /tmp/${SERVICE_NAME}.service
-sleep 15
-${FLEETCTL_COMMAND} start ${SERVICE_NAME}.service
+# Remove temporary job file.
+rm -f /tmp/${KUBE_DEPLOYMENT_NAME}-new.yml
