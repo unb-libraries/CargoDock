60f8850a1b656d4b026e9508d8fa238eef2b2a41
Jacob Sanford <jsanford@unb.ca>
2017-06-08 14:51:14 -0300

    Create cargoDock.sh

--- cargoDock.sh
+++ cargoDock.sh
@@ -61,7 +61,7 @@ IMAGE_SHA256_HASH=$(docker pull ${AMAZON_ECR_URI}/${SERVICE_NAME}:${BUILD_BRANCH
 ## Deploy.
 ##
 # Notification.
-echo "SHA of ${AMAZON_ECR_URI}/${SERVICE_NAME}:${BUILD_BRANCH} appears to be ${IMAGE_SHA256_HASH}, deploying."
+echo "\n\nSHA of ${AMAZON_ECR_URI}/${SERVICE_NAME}:${BUILD_BRANCH} appears to be ${IMAGE_SHA256_HASH}, deploying.\n\n"
 
 # Update image hash to latest build.
 kubectl get deployment ${KUBE_DEPLOYMENT_NAME} --namespace=${BUILD_BRANCH} -o=yaml | sed "s|\(^\s*\)image: .*|\1image: $AMAZON_ECR_URI/$SERVICE_NAME@$IMAGE_SHA256_HASH|g" > /tmp/${KUBE_DEPLOYMENT_NAME}-new.yml
