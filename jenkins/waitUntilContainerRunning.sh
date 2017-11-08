2866573ea9848ad1f51de0cab3f7b9812e42dd14
Jacob Sanford <jsanford@unb.ca>
2017-11-08 12:40:30 -0400

    Tweak logic of container Run checking

--- jenkins/waitUntilContainerRunning.sh
+++ jenkins/waitUntilContainerRunning.sh
@@ -2,16 +2,15 @@
 #
 # Wait until Kubernetes reports the service pod as running.
 MAX_CONNECT_RETRIES=5
-CONNECT_RETRY_COUNT=0
 CONNECT_RETRY_INTERVAL=15
 
+CONNECT_RETRY_COUNT=0
 echo "Checking if pod is running..."
-until [ ${CONNECT_RETRY_COUNT} -ge ${MAX_CONNECT_RETRIES} ]
-do
-  POD_STATUS=$(kubectl describe pod $KUBE_DEPLOYMENT_NAME --namespace=$BRANCH | grep 'Status:' | awk '{ print $2 }')
+until [ ${CONNECT_RETRY_COUNT} -ge ${MAX_CONNECT_RETRIES} ]; do
+  POD_STATUS=$(kubectl describe pod $KUBE_DEPLOYMENT_NAME --namespace=$BRANCH | grep 'Status:' | awk '{ printf $2 }')
   if [[ "$POD_STATUS" == "Running" ]]; then
     echo "Pod Running..."
-    break
+    break 2
   else
     CONNECT_RETRY_COUNT=$[${CONNECT_RETRY_COUNT}+1]
     echo "${SERVICE_NAME} pod is in state ${POD_STATUS}. Waiting [${CONNECT_RETRY_COUNT}/${MAX_CONNECT_RETRIES}] in ${CONNECT_RETRY_INTERVAL}(s) "
