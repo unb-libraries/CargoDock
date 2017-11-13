11e89c3257ab34ad51a25a576fbdc9060be610e9
Jacob Sanford <jsanford@unb.ca>
2017-11-13 09:17:01 -0400

    Correct image name variable

--- travis/buildInstanceForTesting.sh
+++ travis/buildInstanceForTesting.sh
@@ -6,7 +6,7 @@ set -e
 if [ "$DOCKER_CACHE_BUILD" != "FALSE" ]; then
   AMAZON_ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
   CACHE_IMAGE_NAME_TAG="${AMAZON_ECR_URI}/${SERVICE_NAME}:${TRAVIS_BRANCH}"
-  docker pull ${CACHE_IMAGE_NAME}
+  docker pull ${CACHE_IMAGE_NAME_TAG}
   docker build --build-arg COMPOSER_DEPLOY_DEV=dev --cache-from ${CACHE_IMAGE_NAME} -t ${SERVICE_NAME}:latest .
 else
   docker build --build-arg COMPOSER_DEPLOY_DEV=dev -t ${SERVICE_NAME}:latest .
