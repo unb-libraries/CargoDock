4a7d095b77c2f9162d3a2527a1c59e78a33043e9
Jacob Sanford <jsanford@unb.ca>
2017-11-13 09:10:43 -0400

    Speed builds further, cache upstream

--- travis/buildInstanceForTesting.sh
+++ travis/buildInstanceForTesting.sh
@@ -3,5 +3,13 @@
 # Build and bring up a local instance of the application using docker-compose.
 set -e
 
-docker build --build-arg COMPOSER_DEPLOY_DEV=dev -t ${SERVICE_NAME}:latest .
+if [ "$DOCKER_CACHE_BUILD" != "FALSE" ]; then
+  AMAZON_ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
+  CACHE_IMAGE_NAME_TAG="${AMAZON_ECR_URI}/${SERVICE_NAME}:${TRAVIS_BRANCH}"
+  docker pull ${CACHE_IMAGE_NAME}
+  docker build --build-arg COMPOSER_DEPLOY_DEV=dev --cache-from ${CACHE_IMAGE_NAME} -t ${SERVICE_NAME}:latest .
+else
+  docker build --build-arg COMPOSER_DEPLOY_DEV=dev -t ${SERVICE_NAME}:latest .
+fi
+
 docker-compose up -d
