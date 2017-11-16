ef6acc4a808946dd46df286bda84b32615f66fdb
Jacob Sanford <jsanford@unb.ca>
2017-11-16 07:49:57 -0400

    Conditonally use upstream image instead of assuming it exists

--- travis/buildInstanceForTesting.sh
+++ travis/buildInstanceForTesting.sh
@@ -6,7 +6,13 @@ set -e
 if [ "$DOCKER_CACHE_BUILD" != "FALSE" ]; then
   AMAZON_ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
   CACHE_IMAGE_NAME_TAG="${AMAZON_ECR_URI}/${SERVICE_NAME}:${TRAVIS_BRANCH}"
-  docker pull ${CACHE_IMAGE_NAME_TAG}
+
+  if docker pull ${CACHE_IMAGE_NAME_TAG} ; then
+    docker build --build-arg COMPOSER_DEPLOY_DEV=dev --cache-from ${CACHE_IMAGE_NAME_TAG} -t ${SERVICE_NAME}:latest .
+  else
+    docker build --build-arg COMPOSER_DEPLOY_DEV=dev -t ${SERVICE_NAME}:latest .
+  fi
+
   docker build --build-arg COMPOSER_DEPLOY_DEV=dev --cache-from ${CACHE_IMAGE_NAME_TAG} -t ${SERVICE_NAME}:latest .
 else
   docker build --build-arg COMPOSER_DEPLOY_DEV=dev -t ${SERVICE_NAME}:latest .
