6112ffb5ec7a63bedc026598f3ab06fe3c094e9d
Jacob Sanford <jsanford@unb.ca>
2017-12-13 11:27:56 -0400

    Do not include email when auth to ecr

--- aws/installAuthAws.sh
+++ aws/installAuthAws.sh
@@ -7,5 +7,5 @@ set -e
 if [[ $DEPLOY_BRANCHES =~ (^|,)"$TRAVIS_BRANCH"(,|$) ]]; then
   pip install --user awscli
   export PATH=$PATH:$HOME/.local/bin
-  eval $(aws ecr get-login --region $AMAZON_ECR_REGION)
+  eval $(aws ecr get-login --no-include-email --region $AMAZON_ECR_REGION)
 fi
