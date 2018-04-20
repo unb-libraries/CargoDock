1577143588d68acbe78ed5768e4e32291ecd45f1
Jacob Sanford <jsanford@unb.ca>
2018-04-20 09:21:27 -0300

    Show logs for all services if error occurs.

--- travis/checkStartupForErrors.sh
+++ travis/checkStartupForErrors.sh
@@ -3,17 +3,18 @@
 # Check the docker-compose instance's log for errors. Returns code 1 an errors
 # is found. Currently only checks instance for the word 'error'.
 STARTUP_LOG=$(docker-compose logs "$SERVICE_NAME")
-
-# Echo the log for travis.
-echo "$STARTUP_LOG"
+LOWER_STARTUP_LOG=${STARTUP_LOG,,}
 
 # If error strings found in startup, exit.
-LOWER_STARTUP_LOG=${STARTUP_LOG,,}
 if [[ $LOWER_STARTUP_LOG == *"error"* ]]; then
-  # Allow the stdout buffer more time to be written to output
-  sleep 10
-
   # Bring down the hammer.
   echo "Error found in container startup."
+  STARTUP_LOG=$(docker-compose logs)
+  echo "$STARTUP_LOG"
+  
+  # Allow the stdout buffer more time to be written to output
+  sleep 10
   exit 1
 fi
+
+echo "$STARTUP_LOG"
