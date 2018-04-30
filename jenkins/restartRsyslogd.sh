a05dabb9728df1ff2ab2ac80fd62c1c8c37a46f9
Jacob Sanford <jsanford@unb.ca>
2018-04-30 08:44:56 -0300

    Add rsyslogd restart script

---
+++ jenkins/restartRsyslogd.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Wait until kube deployment logs show the finished marker.
+set -e
+
+KUBE_DEPLOYMENT_NAME=$(echo $SERVICE_NAME | sed 's/\./-/g')
+POD_NAME=$(kubectl get pods --namespace=$BRANCH --sort-by=.status.startTime -l tier=$KUBE_DEPLOYMENT_NAME --no-headers | tac | awk '{ print $1 }' | head -n 1)
+
+echo "Restarting rsyslogd in $POD_NAME..."
+kubectl exec $POD_NAME --namespace=$BRANCH -- killall -9 rsyslogd
+sleep 2
+kubectl exec $POD_NAME --namespace=$BRANCH -- /usr/sbin/rsyslogd -f /etc/rsyslog.conf
