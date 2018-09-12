1956cc0d19e789d79ac8284ee40f5a878f6c4e80
Jacob Sanford <jsanford@unb.ca>
2018-09-12 13:24:03 -0300

    Update docker-compose in travis to 1.22.0

--- travis/installDockerCompose.sh
+++ travis/installDockerCompose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Install docker compose on travis instance.
-DOCKER_COMPOSE_VERSION='1.16.1'
+DOCKER_COMPOSE_VERSION='1.22.0'
 
 set -e
 curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
