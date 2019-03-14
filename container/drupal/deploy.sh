1250c109ede5e26f005e7da9512b9de30b4086cc
jsanford <jsanford@unb.ca>
2019-03-14 13:39:58 -0300

    CDOCK-5 Copy entire path, not just scripts

--- container/drupal/deploy.sh
+++ container/drupal/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 curl -OL http://github.com/unb-libraries/CargoDock/archive/master.zip
 unzip master.zip
-mv CargoDock-master/container/drupal/*.sh /scripts/
+mv CargoDock-master/container/drupal/* /scripts/
 rm -rf master.zip CargoDock-master
-
