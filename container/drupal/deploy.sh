37c75008733e388b16a8ffc865dfb918fc801bb0
Jacob Sanford <jsanford@unb.ca>
2019-03-14 14:06:18 -0300

    RSync scripts instead of moving

--- container/drupal/deploy.sh
+++ container/drupal/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 curl -OL http://github.com/unb-libraries/CargoDock/archive/master.zip
 unzip master.zip
-mv CargoDock-master/container/drupal/* /scripts/
+rsync -a CargoDock-master/container/drupal/ /scripts/
 rm -rf master.zip CargoDock-master
