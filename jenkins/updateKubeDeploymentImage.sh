93516705cbfd9bd76f9066a0b742ac6202c62f7c
Jacob Sanford <jsanford@unb.ca>
2019-04-18 11:38:34 -0300

    IN-177 Echo update command before executing

--- jenkins/updateKubeDeploymentImage.sh
+++ jenkins/updateKubeDeploymentImage.sh
@@ -12,4 +12,6 @@ KUBE_DEPLOYMENT_NAME=$(echo $SERVICE_NAME | sed 's/\./-/g')
 
 # Update image hash to tag.
 echo "Updating image for $SERVICE_NAME - $BRANCH to $IMAGE_TAG in Kubernetes..."
-kubectl set image --record deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_NAME}=$AMAZON_ECR_URI/$SERVICE_NAME:$IMAGE_TAG --namespace=${BRANCH}
+UPDATE_COMMAND="kubectl set image --record deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_NAME}=$AMAZON_ECR_URI/$SERVICE_NAME:$IMAGE_TAG --namespace=${BRANCH}"
+echo "$UPDATE_COMMAND"
+$UPDATE_COMMAND
