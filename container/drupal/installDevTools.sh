cdc5e56b4b80e822af0dd0194caf3ded08ef3e2a
jsanford <jsanford@unb.ca>
2019-04-25 10:00:25 -0300

    Install blackfire on dev instances

--- container/drupal/installDevTools.sh
+++ container/drupal/installDevTools.sh
@@ -17,4 +17,7 @@ if [ "$DRUPAL_COMPOSER_DEV" == "dev" ]; then
   # Twig settings
   sed -i "s|debug: false|debug: true|g" ${DRUPAL_BUILD_TMPROOT}/sites/default/services.yml
   sed -i "s|cache: true|cache: false|g" ${DRUPAL_BUILD_TMPROOT}/sites/default/services.yml
+
+  # Install Blackfire probe
+  /scripts/installBlackfire.sh
 fi
