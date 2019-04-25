867f4ec6dee1bb5a7b5041bdd47fa89f8ba2632a
jsanford <jsanford@unb.ca>
2019-04-25 10:04:18 -0300

    Add new build arg to install blackfire

--- container/drupal/installDevTools.sh
+++ container/drupal/installDevTools.sh
@@ -17,7 +17,10 @@ if [ "$DRUPAL_COMPOSER_DEV" == "dev" ]; then
   # Twig settings
   sed -i "s|debug: false|debug: true|g" ${DRUPAL_BUILD_TMPROOT}/sites/default/services.yml
   sed -i "s|cache: true|cache: false|g" ${DRUPAL_BUILD_TMPROOT}/sites/default/services.yml
+fi
 
-  # Install Blackfire probe
+# Install Blackfire probe.
+INSTALL_BLACKFIRE_PROBE="${1:-FALSE}"
+if [[ "$INSTALL_BLACKFIRE_PROBE" == "TRUE" ]]; then
   /scripts/installBlackfire.sh
 fi
