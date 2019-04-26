62e247a62fa605bc9ba9c7e47717eee55d658761
jsanford <jsanford@unb.ca>
2019-04-26 11:46:07 -0300

    Remove namespacing from container scripts

--- container/deploy.sh
+++ container/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 curl -OL http://github.com/unb-libraries/CargoDock/archive/drupal-8.x-1.x.zip
 unzip drupal-8.x-1.x.zip
-rsync -a CargoDock-drupal-8.x-1.x/container/drupal/ /scripts/
+rsync -a CargoDock-drupal-8.x-1.x/container/ /scripts/
 rm -rf drupal-8.x-1.x.zip CargoDock-drupal-8.x-1.x
