e45ca08a0cf48c989ce556cc338bbe6a9120ae5d
jsanford <jsanford@unb.ca>
2019-04-26 14:58:36 -0300

    Remove testing if not dev

--- container/cleanupBuild.sh
+++ container/cleanupBuild.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
+# Dev/NoDev
+DRUPAL_COMPOSER_DEV="${1:-no-dev}"
+
+# Remove testing, etc if not dev.
+if [ "$DRUPAL_COMPOSER_DEV" != "dev" ]; then
+  rm -rf $DRUPAL_TESTING_ROOT
+fi
+
 # Clear composer cache
 if [ "$COMPOSER_CLEAR_CACHE" != "FALSE" ]; then
   composer clear-cache
