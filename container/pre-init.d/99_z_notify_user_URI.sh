ebd55cf9831f883a8afd7ce6190b03e7653c01bd
Jacob Sanford <jsanford@unb.ca>
2019-04-27 12:35:38 -0300

    Use printf for consistency

--- container/pre-init.d/99_z_notify_user_URI.sh
+++ container/pre-init.d/99_z_notify_user_URI.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 if [[ -n "$LOCAL_HOSTNAME" ]] && [[ -n "$LOCAL_PORT" ]]; then
-  echo "\nVisit your instance at:"
-  echo "http://$LOCAL_HOSTNAME"
+  printf "\nVisit your instance at:"
+  printf "http://$LOCAL_HOSTNAME"
 
-  echo "\nLog-in to your instance via:"
+  printf "\nLog-in to your instance via:"
   /scripts/drupalUli.sh
   if nslookup mailhog &> /dev/null; then
     MAILHOG_PORT=$((LOCAL_PORT+1000))
-    echo "\nView sent mail at:"
-    echo "http://$LOCAL_HOSTNAME:$MAILHOG_PORT"
+    printf "\nView sent mail at:"
+    printf "http://$LOCAL_HOSTNAME:$MAILHOG_PORT"
   fi
 fi
