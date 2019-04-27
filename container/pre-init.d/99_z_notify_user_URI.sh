762c763b39e1c592166e3fa840c7bebaa60ccec5
Jacob Sanford <jsanford@unb.ca>
2019-04-27 12:56:06 -0300

    Tidy up spacing

--- container/pre-init.d/99_z_notify_user_URI.sh
+++ container/pre-init.d/99_z_notify_user_URI.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 if [[ -n "$LOCAL_HOSTNAME" ]] && [[ -n "$LOCAL_PORT" ]]; then
-  printf "\nVisit your instance at:"
-  printf "http://$LOCAL_HOSTNAME"
+  printf "\n\nVisit your instance at:"
+  printf "\nhttp://$LOCAL_HOSTNAME"
 
-  printf "\nLog-in to your instance via:"
+  printf "\n\nLog-in to your instance via:\n"
   /scripts/drupalUli.sh
   if nslookup mailhog &> /dev/null; then
     MAILHOG_PORT=$((LOCAL_PORT+1000))
-    printf "\nView sent mail at:"
-    printf "http://$LOCAL_HOSTNAME:$MAILHOG_PORT"
+    printf "\n\nView sent mail at:"
+    printf "\nhttp://$LOCAL_HOSTNAME:$MAILHOG_PORT"
   fi
 fi
