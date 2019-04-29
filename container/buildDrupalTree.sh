ed6826e2704d0ba71b21c77db62a3f2f10aeddd1
jsanford <jsanford@unb.ca>
2019-04-29 14:29:57 -0300

    Honor Rsync flags

--- container/buildDrupalTree.sh
+++ container/buildDrupalTree.sh
@@ -22,18 +22,18 @@ rm -rf ${DRUPAL_BUILD_TMPROOT}/profiles/defaultd
 mkdir -p "${DRUPAL_BUILD_TMPROOT}/profiles/${DRUPAL_SITE_ID}"
 
 # Copy config from core install profile for current version of Drupal.
-rsync -a "${DRUPAL_BUILD_TMPROOT}/core/profiles/${DRUPAL_BASE_PROFILE}/config" "${DRUPAL_BUILD_TMPROOT}/profiles/${DRUPAL_SITE_ID}/"
+rsync -a ${RSYNC_FLAGS} "${DRUPAL_BUILD_TMPROOT}/core/profiles/${DRUPAL_BASE_PROFILE}/config" "${DRUPAL_BUILD_TMPROOT}/profiles/${DRUPAL_SITE_ID}/"
 
 # Copy additional configs provided by dockworker.
 ADDITIONAL_CONFIG_DIR="/scripts/data/profiles/${DRUPAL_BASE_PROFILE}/config"
 if [[ -d "$ADDITIONAL_CONFIG_DIR" ]]; then
-  rsync -a ${ADDITIONAL_CONFIG_DIR} ${DRUPAL_BUILD_TMPROOT}/profiles/${DRUPAL_SITE_ID}/
+  rsync -a ${RSYNC_FLAGS} ${ADDITIONAL_CONFIG_DIR} ${DRUPAL_BUILD_TMPROOT}/profiles/${DRUPAL_SITE_ID}/
 fi
 
 # Move local profile from repo to build root, overwriting.
 cd ${DRUPAL_BUILD_TMPROOT}
-rsync -a --remove-source-files ${TMP_DRUPAL_BUILD_DIR}/${DRUPAL_SITE_ID} ${DRUPAL_BUILD_TMPROOT}/profiles/
+rsync -a ${RSYNC_FLAGS} --remove-source-files ${TMP_DRUPAL_BUILD_DIR}/${DRUPAL_SITE_ID} ${DRUPAL_BUILD_TMPROOT}/profiles/
 
 # Move settings files into build location.
 rm -rf ${DRUPAL_BUILD_TMPROOT}/sites/all/settings
-rsync -a --remove-source-files ${TMP_DRUPAL_BUILD_DIR}/settings ${DRUPAL_BUILD_TMPROOT}/sites/all/
+rsync -a ${RSYNC_FLAGS} --remove-source-files ${TMP_DRUPAL_BUILD_DIR}/settings ${DRUPAL_BUILD_TMPROOT}/sites/all/
