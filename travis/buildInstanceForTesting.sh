b9bebd9cabe876278e63a2108653109696fbf5f0
jsanford <jsanford@unb.ca>
2019-05-03 09:52:10 -0300

    Simplify logic re build caching in travis

--- travis/buildInstanceForTesting.sh
+++ travis/buildInstanceForTesting.sh
@@ -2,23 +2,18 @@
 #
 # Build and bring up a local instance of the application using docker-compose.
 set -e
+echo "Building instance for testing..."
 
-if [ "$DOCKER_CACHE_BUILD" != "FALSE" ]; then
-  AMAZON_ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
-  CACHE_IMAGE_NAME_TAG="${AMAZON_ECR_URI}/${SERVICE_NAME}:${TRAVIS_BRANCH}"
+AMAZON_ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
+CACHE_IMAGE_NAME_TAG="${AMAZON_ECR_URI}/${SERVICE_NAME}:${TRAVIS_BRANCH}"
 
-  if docker pull ${CACHE_IMAGE_NAME_TAG} ; then
-    docker build --build-arg COMPOSER_DEPLOY_DEV=dev --cache-from ${CACHE_IMAGE_NAME_TAG} -t ${SERVICE_NAME}:latest .
-  else
-    docker build --build-arg COMPOSER_DEPLOY_DEV=dev -t ${SERVICE_NAME}:latest .
-  fi
-
+if [[ "$DOCKER_CACHE_BUILD" != "FALSE" ]] && docker pull ${CACHE_IMAGE_NAME_TAG}; then
   docker build --build-arg COMPOSER_DEPLOY_DEV=dev --cache-from ${CACHE_IMAGE_NAME_TAG} -t ${SERVICE_NAME}:latest .
 else
   docker build --build-arg COMPOSER_DEPLOY_DEV=dev -t ${SERVICE_NAME}:latest .
 fi
 
-if [ "$DEBUG_CONTAINER_START" == "TRUE" ]; then
+if [[ "$DEBUG_CONTAINER_START" == "TRUE" ]]; then
   echo "Starting container, debug mode activated - Testing WILL FAIL"
   docker-compose up
 else
