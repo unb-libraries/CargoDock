9c7414ee6f0bb1e8d89bc19d43061c2f1b9e9218
jsanford <jsanford@unb.ca>
2019-05-16 12:00:54 -0300

    Add ability to filter expected errors in Jenkins

--- jenkins/checkDeploymentStartupForError.sh
+++ jenkins/checkDeploymentStartupForError.sh
@@ -16,7 +16,13 @@ echo "$POD_LOGS"
 
 # If error strings found in startup, exit.
 LOWER_POD_LOGS=${POD_LOGS,,}
-if [[ $LOWER_POD_LOGS == *"error"* ]]; then
+
+# Exceptions
+EXCEPTIONS=("ERROR 1045")
+EXCEPTION_PATTERNS=$(echo ${args[@]}|tr " " "|")
+LOGS_EXCEPTIONS_REMOVED=$(echo "$LOWER_POD_LOGS" | grep -vEow "$EXCEPTION_PATTERNS")
+
+if [[ $LOGS_EXCEPTIONS_REMOVED == *"error"* ]]; then
   echo "Error found in container startup."
   exit 1
 fi
