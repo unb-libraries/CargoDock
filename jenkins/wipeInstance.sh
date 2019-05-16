964f43f1920e5665bf3f39e1659e1ae9ece290e0
jsanford <jsanford@unb.ca>
2019-05-16 08:28:53 -0300

    Add instance wiping script

---
+++ jenkins/wipeInstance.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Destroy all persistent data in a deployment and re-install.
+if [[ -z "$WIPE_SERVICE_NAME" ]]
+then
+  echo "Service name not set!"
+  exit 1
+fi
+
+if [[ -z "$WIPE_BRANCH" ]]
+then
+  echo "Branch name not set!"
+  exit 1
+fi
+
+KUBE_DEPLOYMENT_NAME=$(echo $SERVICE_NAME | sed 's/\./-/g')
+CONNECT_RETRY_COUNT=0
+POD_NAME=$(kubectl get pods --namespace=$BRANCH --sort-by=.status.startTime -l instance=$SERVICE_NAME --no-headers | tac | awk '{ print $1 }' | head -n 1)
+KUBE_EXEC="kubectl exec -it "$POD_NAME" --namespace="$BRANCH" --"
+SITE_ID=$($KUBE_EXEC sh -lc "echo \$DRUPAL_SITE_ID")
+
+echo "Wiping all data for $SERVICE_NAME:$BRANCH"
+echo "Kubernetes Pod ID: $POD_NAME"
+
+# Drop database
+echo "Dropping database..."
+$KUBE_EXEC sh -lc "drush --yes --root=/app/html sql-drop"
+
+# Delete Files
+echo "Deleting filesystem..."
+$KUBE_EXEC sh -lc "rm -rf /app/html/sites/default/*"
+$KUBE_EXEC sh -lc "rm -rf /app/html/sites/default/.*"
+
+# Kill Nginx, causing pull and redeploy.
+echo "Restarting service..."
+$KUBE_EXEC sh -lc "nginx -s stop"
