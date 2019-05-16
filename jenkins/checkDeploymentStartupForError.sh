f4580fa5d5b0dec5ab3a442d91661177011af446
jsanford <jsanford@unb.ca>
2019-05-16 12:19:06 -0300

    Debug error checking

--- jenkins/checkDeploymentStartupForError.sh
+++ jenkins/checkDeploymentStartupForError.sh
@@ -21,6 +21,8 @@ LOWER_POD_LOGS=${POD_LOGS,,}
 LOGS_EXCEPTIONS_REMOVED=$(echo "$LOWER_POD_LOGS" | grep -v 'ERROR 1045')
 
 if [[ $LOGS_EXCEPTIONS_REMOVED == *"error"* ]]; then
+  echo "**"
+  echo "$LOGS_EXCEPTIONS_REMOVED"
   echo "Error found in container startup."
   exit 1
 fi
