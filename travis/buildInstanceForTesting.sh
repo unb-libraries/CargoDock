cd5d7d36f4d8783cfbfe6623e46f30330671a5b9
jsanford <jsanford@unb.ca>
2019-06-26 12:46:50 -0300

    CDOCK-8 Avoid caching from upstream on first build

--- travis/buildInstanceForTesting.sh
+++ travis/buildInstanceForTesting.sh
@@ -4,14 +4,8 @@
 set -e
 echo "Building instance for testing..."
 
-AMAZON_ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
-CACHE_IMAGE_NAME_TAG="${AMAZON_ECR_URI}/${SERVICE_NAME}:${TRAVIS_BRANCH}"
-
-if [[ "$DOCKER_CACHE_BUILD" != "FALSE" ]] && docker pull ${CACHE_IMAGE_NAME_TAG}; then
-  docker build --build-arg COMPOSER_DEPLOY_DEV=dev --cache-from ${CACHE_IMAGE_NAME_TAG} -t ${SERVICE_NAME}:latest .
-else
-  docker build --build-arg COMPOSER_DEPLOY_DEV=dev -t ${SERVICE_NAME}:latest .
-fi
+# Caching this step from upstream was causing problems with old CargoDock.
+docker build --build-arg COMPOSER_DEPLOY_DEV=dev -t ${SERVICE_NAME}:latest .
 
 if [[ "$DEBUG_CONTAINER_START" == "TRUE" ]]; then
   echo "Starting container, debug mode activated - Testing WILL FAIL"
