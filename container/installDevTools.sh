f3f6df2bff9600d3af3a8a936f64777488dcb852
jsanford <jsanford@unb.ca>
2019-07-09 11:40:57 -0300

    Rely on upstream to manage additional settings and services

--- container/installDevTools.sh
+++ container/installDevTools.sh
@@ -5,18 +5,6 @@ DRUPAL_COMPOSER_DEV="${1:-no-dev}"
 
 # Dev Addons.
 echo "DRUPAL_COMPOSER_DEV = $DRUPAL_COMPOSER_DEV"
-if [ "$DRUPAL_COMPOSER_DEV" == "dev" ]; then
-  echo "Installing Dev Tools..."
-
-  # Copy default services if no services.yml has been provided
-  if [[ ! -f "${DRUPAL_BUILD_TMPROOT}/sites/default/services.yml" ]]; then
-    cp "${DRUPAL_BUILD_TMPROOT}/sites/default/default.services.yml" "${DRUPAL_BUILD_TMPROOT}/sites/default/services.yml"
-  fi
-
-  # Twig settings
-  sed -i "s|debug: false|debug: true|g" ${DRUPAL_BUILD_TMPROOT}/sites/default/services.yml
-  sed -i "s|cache: true|cache: false|g" ${DRUPAL_BUILD_TMPROOT}/sites/default/services.yml
-fi
 
 # Blackfire/NoBlackfire.
 INSTALL_BLACKFIRE_PROBE="${2:-FALSE}"
