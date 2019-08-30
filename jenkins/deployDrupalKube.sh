70b5e25ec1f40fe7900592e855fcf4d47f300bb1
jsanford <jsanford@unb.ca>
2019-08-30 15:27:11 -0300

    Change branch for script pulling in deploy script

--- jenkins/deployDrupalKube.sh
+++ jenkins/deployDrupalKube.sh
@@ -2,10 +2,9 @@
 #
 # Standard k8s deploy Drupal 8 sequence at UNBLibraries.
 set -e
-curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/master/jenkins/updateKubeDeploymentImage.sh | bash
-curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/master/jenkins/pauseForDeploymentCompletion.sh | bash
-curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/master/jenkins/waitUntilContainerRunning.sh | bash
-curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/master/jenkins/checkForFinishedMarker.sh | bash
-curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/master/jenkins/checkDeploymentStartupForError.sh | bash
-curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/master/jenkins/restartRsyslogd.sh | bash
-
+curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/drupal-8.x-1.x/jenkins/updateKubeDeploymentImage.sh | bash
+curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/drupal-8.x-1.x/jenkins/pauseForDeploymentCompletion.sh | bash
+curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/drupal-8.x-1.x/jenkins/waitUntilContainerRunning.sh | bash
+curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/drupal-8.x-1.x/jenkins/checkForFinishedMarker.sh | bash
+curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/drupal-8.x-1.x/jenkins/checkDeploymentStartupForError.sh | bash
+curl -s https://raw.githubusercontent.com/unb-libraries/CargoDock/drupal-8.x-1.x/jenkins/restartRsyslogd.sh | bash
