42baeec5544e1db9ce004205320f0ed9c3b3eeee
jsanford <jsanford@unb.ca>
2019-08-30 15:52:29 -0300

    Restart rsyslogd on all pods in deployment

--- jenkins/restartRsyslogd.sh
+++ jenkins/restartRsyslogd.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 #
-# Wait until kube deployment logs show the finished marker.
+# Restart rsyslogd to send logs upstream.
 set -e
 
 KUBE_DEPLOYMENT_NAME=$(echo $SERVICE_NAME | sed 's/\./-/g')
-POD_NAME=$(kubectl get pods --namespace=$BRANCH --sort-by=.status.startTime -l instance=$SERVICE_NAME --no-headers | tac | awk '{ print $1 }' | head -n 1)
+POD_NAMES=$(kubectl get pods --namespace=$BRANCH --sort-by=.status.startTime -l instance=$SERVICE_NAME --no-headers | grep 'Running' | tac | awk '{ print $1 }')
+POD_COUNTER=0
 
-echo "Restarting rsyslogd in $POD_NAME..."
-kubectl exec $POD_NAME --namespace=$BRANCH -- killall -9 rsyslogd
-sleep 2
-kubectl exec $POD_NAME --namespace=$BRANCH -- /usr/sbin/rsyslogd -f /etc/rsyslog.conf
+# Loop over all this deployment's pods.
+while IFS_PODS= read -r POD_NAME
+do
+  POD_COUNTER=$((POD_COUNTER+1))
+  echo "Restarting rsyslogd in $POD_NAME..."
+  kubectl exec $POD_NAME --namespace=$BRANCH -- killall -9 rsyslogd
+  sleep 2
+  kubectl exec $POD_NAME --namespace=$BRANCH -- /usr/sbin/rsyslogd -f /etc/rsyslog.conf
+done < <(printf '%s\n' "$POD_NAMES")
