3e888a1eeb659abcf3468677049ab99598ecf075
jsanford <jsanford@unb.ca>
2019-09-03 15:32:08 -0300

    Move checking startup / wait to main script to allow failure

--- travis/beforeScript.sh
+++ travis/beforeScript.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env sh
 CargoDock/travis/buildInstanceTheme.sh
-CargoDock/travis/buildInstanceForTesting.sh
-CargoDock/travis/waitForDeploy.sh
-CargoDock/travis/checkStartupForErrors.sh
--- travis/testInstance.sh
+++ travis/testInstance.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+# Bring up container.
+CargoDock/travis/buildInstanceForTesting.sh
+CargoDock/travis/waitForDeploy.sh
+CargoDock/travis/checkStartupForErrors.sh
+
 # Run container tests.
 docker ps
 docker exec -i -t ${SERVICE_NAME} /scripts/runTests.sh
