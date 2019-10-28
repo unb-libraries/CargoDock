c627c4a69765517cb554fd5067260bebf83122b4
Jacob Sanford <jsanford@unb.ca>
2019-10-28 08:37:31 -0300

    Enable simpletest before unit tests.

--- container/runUnitTests.sh
+++ container/runUnitTests.sh
@@ -2,7 +2,9 @@
 set -e
 
 # PHPUnit Tests
+$DRUSH en simpletest
 for CUR_TEST_MODULE in $DRUPAL_UNIT_TEST_MODULES
 do
   su nginx -s /bin/sh -c "php /app/html/core/scripts/run-tests.sh --php /usr/bin/php --module $CUR_TEST_MODULE"
 done
+$DRUSH pmu simpletest
