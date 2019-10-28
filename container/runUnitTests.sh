7763c5578e148df49157c95394196f1be571df43
Jacob Sanford <jsanford@unb.ca>
2019-10-28 09:46:15 -0300

    Make PHPUnit tests verbose

--- container/runUnitTests.sh
+++ container/runUnitTests.sh
@@ -5,6 +5,6 @@ set -e
 $DRUSH en simpletest
 for CUR_TEST_MODULE in $DRUPAL_UNIT_TEST_MODULES
 do
-  su nginx -s /bin/sh -c "php /app/html/core/scripts/run-tests.sh --php /usr/bin/php --module $CUR_TEST_MODULE"
+  su nginx -s /bin/sh -c "php /app/html/core/scripts/run-tests.sh --php /usr/bin/php --verbose --module $CUR_TEST_MODULE"
 done
 $DRUSH pmu simpletest
